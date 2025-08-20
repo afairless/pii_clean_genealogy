27368635bff795f0b951a07c9cfbdb68a5816cae
gv <afairless@gmail.com>
2025-08-20 18:59:07 -0400

    **feat: add XML genealogy cleaning and update input handling**
    - Add functions to extract and clean top-level tags in genealogy XML trees, removing phone numbers and emails.
    - Refactor main.py to process XML files instead of text files.

--- src/main.py
+++ src/main.py
@@ -1,26 +1,40 @@
 #! /usr/bin/env python3
 
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 from src.processing import (
-    read_text_file,
-    write_text_file,
-    clean_phone_email,
+    extract_top_tag_genealogy_xml_tree,
+    clean_genealogy_xml_tree,
     )
 
 
 def main():
 
+
+    # SET INPUT AND OUTPUT FILE PATHS
+    ##################################################
+
     input_path = Path.cwd() / 'input'
-    input_filepath = input_path / 'input.txt'
+    input_filename = 'input.xml'
+    input_filepath = input_path / input_filename 
 
     output_path = Path.cwd() / 'output'
-    output_filepath = output_path / 'output.txt'
+    output_filepath = output_path / input_filename
 
-    txt = read_text_file(input_filepath)
-    cleaned_txt = clean_phone_email(txt)
 
-    write_text_file(output_filepath, cleaned_txt)
+    # SET INPUT AND OUTPUT FILE PATHS
+    ##################################################
+
+    tree = ET.parse(input_filepath)
+    root = tree.getroot()
+
+    top_level_tag = 'text'
+    text_tag = extract_top_tag_genealogy_xml_tree(root, top_level_tag)
+    clean_root = clean_genealogy_xml_tree(root, text_tag)
+
+    clean_tree = ET.ElementTree(clean_root)
+    clean_tree.write(output_filepath, encoding='utf-8', xml_declaration=True)
 
 
 if __name__ == '__main__':
--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import scrubadub
+import xml.etree.ElementTree as ET
 
 
 def read_text_file(file_path):
@@ -36,4 +37,44 @@ def clean_phone_email(txt: str) -> str:
     cleaned_txt = scrubber.clean(txt)
 
     return cleaned_txt
+
+
+def extract_top_tag_genealogy_xml_tree(
+    root: ET.Element, top_level_tag: str) -> str:
+    """
+    Extract the top-level tag from a genealogy XML tree
+    """
+
+    namespace = root.tag.split('}')[0].strip('{')
+    tag = f'{{{namespace}}}{top_level_tag}'
+
+    return tag
+
+
+def clean_genealogy_xml_tree(
+    root: ET.Element, top_level_tag: str) -> ET.Element:
+    """
+    Clean the genealogy XML tree by removing phone numbers and email addresses
+    Text fields in the tree are filtered before applying 'clean_phone_email' to
+        avoid false positives, especially with the email detector, which 
+        incorrectly identifies email addresses in text using the word 'at'.
+    """
+
+    for e in root.iter():
+        if e.tag == top_level_tag:
+            if e.text is not None:
+
+                if 'txtForward' in e.text:
+                    e.text = clean_phone_email(e.text)
+
+                elif (
+                    'from:' in e.text.lower() and 
+                    # 'to:' in e.text.lower() and
+                    'date:' in e.text.lower() and
+                    'subject:' in e.text.lower()):
+
+                    e.text = clean_phone_email(e.text)
+
+    return root
+
 
